2a02908da04b7328fbf514760c4fcb886257fed2
VladiTNT <vladnastasie2008@gmail.com>
2026-09-11 19:15:30 +0300

    fastfetch config added

---
+++ fastfetch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Variables
+NAME="Fastfetch"
+SRC="./fastfetch"
+TARGET="$HOME/.config/fastfetch"
+# If there is a config file in target.
+if [ -d "$TARGET" ] || [ -L "$TARGET" ]; then
+    # Format backup path
+    BACKUP="${TARGET}_$(date +%Y-%m-%d_%H:%M:%S).backup"
+
+    # Backup the existing config before copying new dotfiles there.
+    cp -rf "$TARGET" "$BACKUP"
+    echo "WARNING! Found existing config at $TARGET, making backup in $BACKUP"
+else
+    # Make target directory if there isn't a config.
+    mkdir -p "$TARGET"
+fi
+# Copy dotfiles over to the target.
+cp -rf "$SRC/." "$TARGET"
+echo "$NAME config installed!"
