09633af30d76722bbdc62bae82334d9f8cff2a6b
VladiTNT <vladnastasie2008@gmail.com>
2026-09-11 18:49:38 +0300

    made changes to install scripts

--- nvim.sh
+++ nvim.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-# Source directory for config.
+# Variables
+NAME="Neovim"
 SRC="./nvim"
-# Target directory for config.
 TARGET="$HOME/.config/nvim"
-
-# If the there is a config at the target path already, make a backup.
+# If there is a config file in target.
 if [ -d "$TARGET" ] || [ -L "$TARGET" ]; then
     # Format backup path
     BACKUP="${TARGET}_$(date +%Y-%m-%d_%H:%M:%S).backup"
-    echo "Note: found existing config at $TARGET, making a backup in $BACKUP"
 
     # Backup the existing config before copying new dotfiles there.
     cp -rf "$TARGET" "$BACKUP"
+    echo "WARNING! Found existing config at $TARGET, making backup in $BACKUP"
 else
     # Make target directory if there isn't a config.
     mkdir -p "$TARGET"
 fi
-
 # Copy dotfiles over to the target.
 cp -rf "$SRC/." "$TARGET"
+echo "$NAME config installed!"
